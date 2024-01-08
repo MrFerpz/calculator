6e0bfa581d134c1a6feaeda66633cb0a8d92826e
Fred <leferpz@gmail.com>
2024-01-08 11:31:15 -0500

    Added IDs to all the buttons, made the display very visible, added (untested) logic to an operate function.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+const body = document.querySelector("body");
+const oneBtn = document.querySelector()
+
 function add(a, b) {
     return (a + b)
 }
@@ -13,3 +16,27 @@ function multiply(a, b) {
 function divide(a, b) {
     return (a / b)
 }
+
+let a;
+let b;
+let operator = "";
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            add(a,b);
+            break
+        case "-":
+            subtract(a,b);
+            break
+        case "x":
+            multiply(a,b);
+            break
+        case "÷":
+            divide(a,b);
+            break
+    }
+}
+
+let display = document.querySelector(".display");
+display.textContent = "yeah";
